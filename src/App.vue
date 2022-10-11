55842eacbd930bbb6bd0f41445cbab28fc548330
Yasaman K <yasaman@m.lo.agency>
2022-10-11 16:28:33 +0330

    style: input and container

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,14 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
+  <div class="container">
+    <header>
+      <h2 class="title2">Create Task</h2>
+      <button class="title2">Done</button>
+    </header>
+    <div class="task-detail">
+      <input placeholder="Add Title ... " name="newTitle" />
+      <textarea placeholder="Add Note ... " />
+    </div>
+  </div>
 </template>
 
 <script>
@@ -10,12 +19,55 @@ export default {
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+.container {
+  margin: 4rem 1rem;
+}
+.title2 {
+  display: inline;
+  padding: 5px 16px;
+  border-bottom: 5px solid rgb(95, 102, 15);
+  border-radius: 10px;
+  text-shadow: 2px 2px rgb(103, 103, 10);
+  color: rgba(255, 255, 0, 0.514);
+  box-shadow: 0px 0px 3px 3px rgb(69, 73, 6);
+  cursor: pointer;
+}
+
+.title2:hover {
+  background-color: rgb(215, 215, 41);
+}
+
+.task-detail {
+  margin-top: 3rem;
+  display: flex;
+  flex-direction: column;
+  width: 70%;
+}
+
+.task-detail div {
+  margin-top: 3rem;
+}
+
+textarea {
+  margin-top: 2rem;
+  height: 10rem;
+  resize: none;
+  padding: 10px 5px;
+}
+
+input {
+  padding: 10px 5px;
+}
+
+button {
+  font-weight: 900;
+  font-size: 24px;
+}
+
+header {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  
 }
 </style>
